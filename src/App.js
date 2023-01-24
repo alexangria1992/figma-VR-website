0592bf7ecb90298f894ef11a82e426624bb08daf
alexangria1992 <alex040892@gmail.com>
2023-01-24 21:08:23 +1300

    Nav and mobile nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import NavMobile from "./components/NavMobile";
 
 function App() {
+  const [navMobile, setNavMobile] = useState(false);
   return (
-    <div>
-      <Header />
+    <div
+      className="relative overflow-hidden before:w-[600px]
+    before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute
+    before:-top-16 before:left-[600px] before:hidden before:lg:flex"
+    >
+      <Header setNavMobile={setNavMobile} />
       <Banner />
+      {/* Mobile Nav */}
+      <div
+        className={`${
+          navMobile ? "right-0" : "-right-full"
+        } fixed top-0 bottom-0 w-48 transition-all `}
+      >
+        <NavMobile setNavMobile={setNavMobile} />
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,21 @@ import Nav from "./Nav";
 
 import { HiMenu } from "react-icons/hi";
 
-const Header = () => {
+const Header = ({ setNavMobile }) => {
   return (
     <header className="py-6 bg-green-300">
       <div className="container mx-auto bg-cyan-700">
-        <div className="bg-purple-300">
+        <div className="bg-purple-300 flex items-center justify-between">
           {/* LOGO */}
           <a href="/">
-            <img src={Logo} alt="" />
+            <img className="h-[30px]" src={Logo} alt="" />
           </a>
           {/* NAV */}
           <Nav />
+          <HiMenu
+            onClick={() => setNavMobile(true)}
+            className="lg:hidden text-3xl text-white cursor-pointer"
+          />
         </div>
       </div>
     </header>
